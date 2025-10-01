fa3aa33ed160dda34adb8a1b0932d15ba1cb835c
Joey Scanga <scangjo@gmail.com>
2025-10-01 14:01:23 -0500

    added hint if no event files found and --raw_bids not specified

--- oceanproc/firstlevel/run_glm.py
+++ oceanproc/firstlevel/run_glm.py
@@ -909,7 +909,11 @@ def main():
                 file_map["events"] = event_files[0]
                 break
             if file_map["events"] is None:
-                raise FileNotFoundError(f"Found no event files in these directories: {', '.join([str(p) for p in event_search_paths])}")
+                raise FileNotFoundError(dedent(f"""
+                    Found no event files in these directories:
+                    {', '.join([str(p) for p in event_search_paths])}
+                    {'(Maybe you forgot --raw_bids?)' if not args.raw_bids else ''}
+                """))
 
             file_map_list.append(file_map)
 
