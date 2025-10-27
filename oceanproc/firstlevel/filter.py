6129d1b27fc62821373aa300dfcbe59b82e80c90
Joey Scanga <scangjo@gmail.com>
2025-10-27 16:24:45 -0500

    filter/nuisance interfaces plus tests

---
+++ oceanproc/firstlevel/filter.py
@@ -0,0 +1,139 @@
+from pathlib import Path
+from copy import deepcopy
+from nipype.interfaces.base import (
+    BaseInterfaceInputSpec,
+    File,
+    OutputMultiObject,
+    SimpleInterface,
+    TraitedSpec,
+    traits,
+)
+import numpy as np
+import numpy.typing as npt
+import nibabel as nib
+from nilearn.signal import (
+    butterworth,
+    _handle_scrubbed_volumes
+)
+
+
+def filter_data(func_data: npt.ArrayLike,
+                mask: npt.ArrayLike,
+                t_r: float,
+                low_pass: float = 0.1,
+                high_pass: float = 0.008,
+                padtype: str = "none",
+                padlen: int = 50):
+    if not any((
+        padtype == "none",
+        padlen is None,
+        (padtype != "zero" and padlen is not None and padlen > 0),
+        (padtype == "zero" and padlen is not None and padlen >= 2),
+    )):
+        raise ValueError(f"Pad length of {padlen} incompatible with pad type {'odd' if padtype is None else padtype}")
+
+    padded_func_data = np.pad(func_data, ((padlen, padlen), (0, 0)), mode='constant', constant_values=0)
+    padded_mask = np.pad(mask, (padlen, padlen), mode='constant', constant_values=True)
+
+    # if the mask is excluding frames, interpolate the censored frames
+    if np.sum(mask) < mask.shape[0]:
+        padded_func_data, _, padded_mask = _handle_scrubbed_volumes(
+            signals=padded_func_data,
+            confounds=None,
+            sample_mask=padded_mask,
+            filter_type="butterworth",
+            t_r=t_r,
+            extrapolate=True
+        )
+
+    if padtype == "zero":
+        filtered_data = butterworth(
+            signals=padded_func_data,
+            sampling_rate=1.0 / t_r,
+            low_pass=low_pass,
+            high_pass=high_pass,
+        )[padlen:-padlen, :]  # remove 0-pad frames on both sides
+        assert filtered_data.shape[0] == func_data.shape[0], "Filtered data must have the same number of timepoints as the original functional data"
+    else:
+        filtered_data = butterworth(
+            signals=func_data,
+            sampling_rate=1.0 / t_r,
+            low_pass=low_pass,
+            high_pass=high_pass,
+            padtype=None if padtype == "none" else padtype,
+            padlen=padlen
+        )
+    return filtered_data
+
+
+class _FilterDataInputSpec(BaseInterfaceInputSpec):
+    in_file = File(
+        exists=True, mandatory=True,
+        desc="Path to unfiltered timeseries (as a .nii, .nii.gz, or .dtseries.nii)."
+    )
+    in_mask = File(
+        exists=True, mandatory=True,
+        desc="Run mask (as a .txt)."
+    )
+    out_file = File(
+        exists=False,
+        desc="Path to filtered timeseries to create."
+    )
+    padtype = traits.Str(
+        "zero",
+        desc="Type of padding to use -- choices: 'odd', 'even', 'constant', 'zero', or 'none'"
+    )
+    padlen = traits.Int(
+        50,
+        desc="Length of pad."
+    )
+
+
+class _FilterDataOutputSpec(TraitedSpec):
+    out_file = OutputMultiObject(
+        File(exists=True),
+        desc="Filtered timeseries."
+    )
+
+
+class FilterData(SimpleInterface):
+    """
+    Generates a nuisance matrix for regression before final GLM.
+    """
+
+    input_spec = _FilterDataInputSpec
+    output_spec = _FilterDataOutputSpec
+
+    def _run_interface(self, runtime):
+        img = nib.load(self.inputs.in_file)
+        func_data = img.get_fdata()
+        orig_shape = deepcopy(func_data.shape)
+        if len(func_data.shape) == 2:  # cifti
+            suffix = ".dtseries.nii"
+        elif len(func_data.shape) == 4:  # nifti
+            suffix = ".nii.gz"
+            func_data = np.reshape(func_data, (-1, func_data.shape[-1])).T
+        mask = np.loadtxt(self.inputs.in_mask)
+
+        filtered_fdata = filter_data(
+            func_data=func_data,
+            mask=mask,
+            t_r=self.inputs.t_r,
+            low_pass=self.inputs.low_pass,
+            high_pass=self.inputs.high_pass,
+            padtype=self.inputs.padtype,
+            padlen=self.inputs.padlen
+        )
+        if suffix == ".dtseries.nii":
+            filtered_img = img.__class__(filtered_fdata,
+                                         header=img.header,
+                                         nifti_header=img.nifti_header)
+            nib.save(filtered_img, self.inputs.out_file)
+            self._results['out_file'] = self.inputs.out_file
+        elif suffix == ".nii.gz":  # reshape back to 4d
+            filtered_fdata = np.reshape(filtered_fdata.T, orig_shape)
+            filtered_img = img.__class__(filtered_fdata, header=img.header)
+            nib.save(filtered_img, self.inputs.out_file)
+            self._results['out_file'] = self.inputs.out_file
+        else:
+            raise RuntimeError("Unexpected data type for functional data")
---
+++ oceanproc/firstlevel/nuisance.py
@@ -0,0 +1,135 @@
+# from nipype import Node, Workflow, Function, MapNode
+# from nipype.pipeline.engine import Node as eNode
+# from nipype.interfaces.io import BIDSDataGrabber
+import pandas as pd
+import numpy as np
+from nipype.interfaces.base import (
+    BaseInterfaceInputSpec,
+    File,
+    InputMultiObject,
+    OutputMultiObject,
+    SimpleInterface,
+    TraitedSpec,
+    isdefined,
+    traits,
+)
+# from . import operations
+# from nipype import config as ncfg
+from pathlib import Path
+# from parser import parse_args
+# from config import all_opts
+
+
+def generate_nuisance_matrix(confounds_file: str,
+                             confounds_columns: list,
+                             output_path: str | Path,
+                             include_trend: bool = False,
+                             fd_threshold: float = None,
+                             volterra_expansion: int = None,
+                             volterra_columns: list = None,):
+    confounds_columns = set(confounds_columns)
+    if fd_threshold:
+        confounds_columns.add("framewise_displacement")
+    if volterra_columns:
+        confounds_columns.update(volterra_columns)
+    suffix = "." + confounds_file.split(".")[-1]
+    if suffix == ".csv":
+        nuisance = pd.read_csv(confounds_file).loc[:,list(confounds_columns)]
+    elif suffix == ".tsv":
+        nuisance = pd.read_csv(confounds_file, sep='\t').loc[:,list(confounds_columns)]
+    else:
+        raise ValueError("Invalid suffix (must be .csv or .tsv)")
+    if "framewise_displacement" in confounds_columns:
+        nuisance.loc[0, "framewise_displacement"] = 0
+        if fd_threshold:
+            b = 0
+            for a in range(len(nuisance)):
+                if nuisance.loc[a, "framewise_displacement"] > fd_threshold:
+                    nuisance[f"spike{b}"] = 0
+                    nuisance.loc[a, f"spike{b}"] = 1
+                    b += 1
+    if include_trend:
+        nuisance["trend"] = np.arange(0, len(nuisance))
+    if volterra_columns and volterra_expansion:
+        for vc in volterra_columns:
+            for lag in range(volterra_expansion):
+                nuisance.loc[:, f"{vc}_{lag + 1}"] = nuisance.loc[:, vc].shift(lag + 1)
+        nuisance.fillna(0, inplace=True)
+
+    if str(output_path).endswith(".tsv"):
+        nuisance.to_csv(output_path, sep='\t')
+        return output_path
+    elif str(output_path).endswith(".csv"):
+        nuisance.to_csv(output_path)
+        return output_path
+
+
+class _GenerateNuisanceMatrixInputSpec(BaseInterfaceInputSpec):
+    in_file = File(
+        exists=True,
+        desc="Run-specific confounds .csv or .tsv."
+    )
+    confounds_columns = traits.List(
+        desc="Variables to use in nuisance regression before final GLM."
+    )
+    do_volterra_expansion = traits.Bool(
+        False,
+    )
+    fd_censoring = traits.Bool(
+        False,
+        desc="Flag to indicate that frames above the framewise displacement threshold should be censored before the GLM."
+    )
+    fd_threshold = traits.Float(
+        0.9,
+        desc="The framewise displacement threshold used when censoring high-motion frames"
+    )
+    is_long = traits.Bool(
+        False,
+        default=False, desc="Whether the events file is already long-formatted."
+    )
+    include_trend = traits.Bool(
+        False,
+        desc="Whether or not to include linear trend in teh nuisance matrix."
+    )
+    minimum_unmasked_neighbors = traits.Int(
+        0,
+        desc="""\
+Minimum number of contiguous unmasked frames on either side of a given frame that's
+required to be under the fd_threshold; any unmasked frame without the required number
+of neighbors will be masked.
+"""
+    )
+    spike_regression = traits.Bool(
+        False,
+        desc="Flag to indicate that framewise displacement spike regression should be included in the nuisance matrix."
+    )
+    out_file = traits.File(
+        exists=False,
+        desc="Path to saved nuisance matrix."
+    )
+    volterra_columns = traits.List(
+        desc="Variables to apply volterra expansion to (must be in confound_columns)"
+    )
+
+
+class _GenerateNuisanceMatrixOutputSpec(TraitedSpec):
+    out_nuisance_matrix = OutputMultiObject(
+        File(exists=True),
+        desc="Outputted nuisance matrix as a file."
+    )
+
+
+class GenerateNuisanceMatrix(SimpleInterface):
+    """
+    Generates a nuisance matrix for regression before final GLM.
+    """
+
+    input_spec = _GenerateNuisanceMatrixInputSpec
+    output_spec = _GenerateNuisanceMatrixOutputSpec
+
+    def _run_interface(self, runtime):
+        self._results['out_nuisance_matrix'] = generate_nuisance_matrix(
+            self.inputs.in_file,
+            self.inputs.confounds_columns,
+            self.inputs.out_file
+        )
